51bad86e6aae0f16e164b17a9207a88ed71b6764
Issei Naruta <naruta@cookpad.com>
2016-03-07 21:47:21 +0900

    Use sqlite3

--- lib/simtick/result.rb
+++ lib/simtick/result.rb
@@ -1,23 +1,32 @@
+require 'sqlite3'
+
 module Simtick
   class Result
     def initialize(ticker_range:100)
       @ticker_range = ticker_range
-      @statuses = Hash.new {|h,k| h[k] = Counter.new }
-      @reqtimes = Counter.new
-      @counts = Counter.new
+      @db = SQLite3::Database.new '/tmp/simtick.db'
+      init_db
     end
 
     def record(ticker:, status:, reqtime:)
       t = ticker / @ticker_range
-      @statuses[status][t] += 1
-      @reqtimes[t] += reqtime
-      @counts[t] += 1
+      @db.execute <<-SQL, ticker, t, status, reqtime
+INSERT INTO `payloads` (`ticker`, `ticker_range`, `status`, `reqtime`) VALUES (?, ?, ?, ?);
+      SQL
     end
 
     def summarize
-      total_requests = @counts.sum
-      avg_reqtime = @reqtimes.sum.to_f / total_requests
-      statuses = Hash[ @statuses.map {|st,c| [st, c.sum] } ]
+      total_requests = @db.execute(
+        'SELECT COUNT(1) FROM `payloads`'
+      ).first.first
+      avg_reqtime = @db.execute(
+        'SELECT AVG(`reqtime`) FROM `payloads`'
+      ).first.first
+      statuses = @db.execute(
+        'SELECT `status`, COUNT(`status`) FROM `payloads` GROUP BY `status`'
+      )
+      statuses = Hash[ statuses ]
+
       {
         total_requests: total_requests,
         avg_reqtime: avg_reqtime,
@@ -25,14 +34,19 @@ module Simtick
       }
     end
 
-    class Counter < Hash
-      def initialize
-        super {|h,k| h[k] = 0 }
-      end
-
-      def sum
-        self.values.inject(0, &:+)
-      end
+    def init_db
+      @db.execute 'DROP TABLE IF EXISTS `payloads`;'
+      @db.execute <<-SQL
+CREATE TABLE `payloads` (
+`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+`ticker` INTEGER NOT NULL,
+`ticker_range` INTEGER NOT NULL,
+`status` INTEGER NOT NULL,
+`reqtime` INTEGER NOT NULL
+)
+      SQL
+      @db.execute 'CREATE INDEX `idx1` ON `payloads` (`ticker_range`, `status`)'
+      @db.execute 'CREATE INDEX `idx2` ON `payloads` (`ticker_range`, `reqtime`)'
     end
   end
 end
